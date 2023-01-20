4f10c33f29142125529fc0e360dbe9e93da58b41
MatiasBcEz <101440051+MatiasBcEz@users.noreply.github.com>
2023-01-20 00:18:05 -0300

    Initial commit

---
+++ src/main/java/cl/duoc/examendb/ExamenDbApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.examendb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenDbApplication.class, args);
+	}
+
+}
