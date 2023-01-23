97bce5d1a546e82fc615ab2eca9fe1a054ab2fe3
MatiasBcEz <101440051+MatiasBcEz@users.noreply.github.com>
2023-01-23 20:21:31 -0300

    asd

--- src/main/java/cl/duoc/examendb/model/entidades/InstrumentoEnt.java
+++ src/main/java/cl/duoc/examendb/model/entidades/InstrumentoEnt.java
@@ -1,7 +1,61 @@
 package cl.duoc.examendb.model.entidades;
 
-import jakarta.annotation.sql.DataSourceDefinition;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "instrumento")
+//@Table(name = "instrumento", schema = "schema_name")
 public class InstrumentoEnt {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "idInstrumento")
+    private int    idInstrumento;
+
+    @Basic
+    @Column (name = "nombre")
+    private String nombre;
+
+    @Basic
+    @Column (name = "marca")
+    private String marca;
+
+    @Basic
+    @Column(name = "id_categoria")
+    private String idCategoria;
+
+    public InstrumentoEnt() {
+    }
+
+    public int getIdInstrumento() {
+        return idInstrumento;
+    }
+    public void setIdInstrumento(int idInstrumento) {
+        this.idInstrumento = idInstrumento;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+
     
 }
