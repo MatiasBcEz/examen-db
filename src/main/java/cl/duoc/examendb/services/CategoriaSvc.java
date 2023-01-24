161e764dc2412bdfa00db8634783ed84e0292774
MatiasBcEz <101440051+MatiasBcEz@users.noreply.github.com>
2023-01-23 23:27:52 -0300

    Update CategoriaSvc.java

--- src/main/java/cl/duoc/examendb/services/CategoriaSvc.java
+++ src/main/java/cl/duoc/examendb/services/CategoriaSvc.java
@@ -2,6 +2,7 @@ package cl.duoc.examendb.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import cl.duoc.examendb.model.entidades.CategoriaEnt;
@@ -10,8 +11,14 @@ import cl.duoc.examendb.model.repositorio.CategoriaRep;
 @Service
 public class CategoriaSvc {
 
+    @Autowired
+    CategoriaRep categoriaRep;
+
     public List<CategoriaEnt> getAll() {
-        return CategoriaEnt.findAll();
+        return categoriaRep.findAll();
     }
 
+    public CategoriaEnt add(CategoriaEnt categoriaEnt){
+        return categoriaRep.save(categoriaEnt);
+    }
 }
